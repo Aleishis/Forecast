41c1c97a3d90c135235d1e98dcc96ecceec8c831
aleishis <mecedesval@gmail.com>
2025-05-06 10:14:59 -0700

    Se inicio el proyecto de Grasshopper

---
+++ src/main/java/mx/itson/grasshopper/entities/Forecast.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.grasshopper.entities;
+
+import java.util.Date;
+import mx.itson.grasshopper.enums.WheaterStatus;
+/**
+ *
+ * @author alelo
+ */
+public class Forecast {
+ 
+    private Date day;
+    private int maxTemperature;
+    private int minTemperature;
+    private WheaterStatus status;
+    
+}
---
+++ src/main/java/mx/itson/grasshopper/enums/WheaterStatus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.grasshopper.enums;
+
+/**
+ *
+ * @author alelo
+ */
+public enum WheaterStatus {
+    
+    SUNNY,
+    SNOW,
+    CLODUY
+    
+}
