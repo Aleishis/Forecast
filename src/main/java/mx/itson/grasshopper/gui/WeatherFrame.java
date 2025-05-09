4fd0920accab225dea71b92c9ad38410d2fe115d
aleishis <mecedesval@gmail.com>
2025-05-09 13:03:41 -0700

    A table is created every time we select a valid JSON file

--- src/main/java/mx/itson/grasshopper/gui/WeatherFrame.java
+++ src/main/java/mx/itson/grasshopper/gui/WeatherFrame.java
@@ -12,6 +12,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import mx.itson.grasshopper.entities.Forecast;
 import mx.itson.grasshopper.entities.Weather;
 
 /**
@@ -37,7 +39,8 @@ public class WeatherFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         btnFile = new javax.swing.JButton();
-        lblCity = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblForecast = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,30 +55,43 @@ public class WeatherFrame extends javax.swing.JFrame {
             }
         });
 
-        lblCity.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        lblCity.setText("...");
+        tblForecast.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Day", "Max Temperature", "Min Temperature"
+            }
+        ));
+        tblForecast.setColumnSelectionAllowed(true);
+        jScrollPane1.setViewportView(tblForecast);
+        tblForecast.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(407, Short.MAX_VALUE)
-                .addComponent(btnFile, javax.swing.GroupLayout.PREFERRED_SIZE, 478, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(395, 395, 395))
             .addGroup(layout.createSequentialGroup()
-                .addGap(474, 474, 474)
-                .addComponent(lblCity, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(204, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnFile, javax.swing.GroupLayout.PREFERRED_SIZE, 478, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(395, 395, 395))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(307, 307, 307))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(51, 51, 51)
                 .addComponent(btnFile, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(94, 94, 94)
-                .addComponent(lblCity, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(340, Short.MAX_VALUE))
+                .addGap(34, 34, 34)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(46, Short.MAX_VALUE))
         );
 
         pack();
@@ -101,7 +117,17 @@ public class WeatherFrame extends javax.swing.JFrame {
                         
                         Weather w = Weather.deserialize(contenido);
                         
-                        lblCity.setText(w.getCity());
+                        DefaultTableModel model = (DefaultTableModel) tblForecast.getModel();
+                        
+                        
+                        model.setRowCount(0);
+                        for (Forecast f : w.getForecast()){
+                            model.addRow(new Object[] {
+                                f.getDay(),
+                                f.getMaxTemperature(),
+                                f.getMinTemperature()
+                            });
+                        }
                         
                     } catch (Exception ex) {
                         System.err.println("Error: " + ex.getMessage());
@@ -147,6 +173,7 @@ public class WeatherFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnFile;
-    private javax.swing.JLabel lblCity;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblForecast;
     // End of variables declaration//GEN-END:variables
 }
